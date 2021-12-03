ad7b41bce505217353b7c4bd1582ab64c89836ce
Yuki Takasu <t.yuki0370@gmail.com>
2021-12-03 11:43:12 +0900

    detail viewの実装

--- sports/urls.py
+++ sports/urls.py
@@ -6,6 +6,7 @@ app_name='sports'
 urlpatterns = [
     path('', views.index, name='index'),
     path('rules/<str:pk>/', views.rules, name='rules'),
-    path('notables/<str:pk>/', views.notables, name='notables'),
+    path('notables/<int:pk>/', views.notables, name='notables'),
+    path('notables/<int:pk>/detail/<str:name>', views.playerDetail, name='player_detail'),
     path('links/<str:pk>/', views.links, name='links'),
 ]
--- sports/views.py
+++ sports/views.py
@@ -1,4 +1,3 @@
-from django.db.models import query
 from django.shortcuts import render
 from .models import Sports, Player, Link
 import random
@@ -28,15 +27,31 @@ def rules(request, pk):
 
 def notables(request, pk):
     sport = Sports.objects.get(pk=pk)
+    players = Player.objects.filter(sports__name=sport.name)
+
     context = {
-        'sports':sport
+        'sports':sport,
+        'players':players
     }
     return render(request, 'sports/notables.html', context)
 
 
+def playerDetail(request, pk, name):
+    sport = Sports.objects.get(pk=pk)
+    player = Player.objects.get(name=name)
+    print(player)
+    context = {
+        'sports': sport,
+        'player': player
+    }
+    return render(request, "sports/detail.html", context)
+
 def links(request, pk):
     sport = Sports.objects.get(pk=pk)
+    links = Link.objects.filter(sports__name=sport.name)
+
     context = {
-        'sports':sport
+        'sports': sport,
+        'links':links
     }
     return render(request, 'sports/link.html', context)
